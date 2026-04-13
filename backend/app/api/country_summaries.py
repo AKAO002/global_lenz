b0d1088279f5d9cc0fbfc99ad963af457c4be2d6
megane575 <rie-fttm0129@gmail.com>
2026-04-13 13:37:25 +0900

    add:各国要約詳細 APIを作成

--- backend/app/api/country_summaries.py
+++ backend/app/api/country_summaries.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from app.services.country_service import (
-    get_country_summaries
+    get_country_summaries,
+    get_country_summary_by_id 
 )
 
 router = APIRouter()
@@ -10,3 +11,13 @@ router = APIRouter()
 def read_country_summaries():
 
     return get_country_summaries()
+
+@router.get("/{summary_id}")
+def read_country_summary(summary_id: int):
+
+    data = get_country_summary_by_id(summary_id)
+
+    if not data:
+        return {"message": "データが見つかりません"}
+
+    return data
--- backend/app/services/country_service.py
+++ backend/app/services/country_service.py
@@ -11,3 +11,13 @@ def get_country_summaries():
     )
 
     return response.data
+
+def get_country_summary_by_id(summary_id: int):
+
+    response = supabase.table(
+        "country_summaries"
+    ).select("*").eq(
+        "id", summary_id
+    ).execute()
+
+    return response.data
