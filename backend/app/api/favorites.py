0aa78c44c1341ae687ad37bfce19ff6f584f421d
megane575 <rie-fttm0129@gmail.com>
2026-04-13 15:20:28 +0900

    add:ネタ帳登録APIを作成

--- backend/app/api/favorites.py
+++ backend/app/api/favorites.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Query
 
 from app.services.favorite_service import (
-    get_favorites
+    get_favorites,
+    create_favorite
 )
+
+from app.schemas.favorite import FavoriteCreate
 
 router = APIRouter()
 
@@ -13,3 +16,14 @@ def read_favorites(
 ):
 
     return get_favorites(user_id)
+
+@router.post("")
+def add_favorite(
+    favorite: FavoriteCreate
+):
+
+    data = create_favorite(
+        favorite.dict()
+    )
+
+    return data
--- backend/app/services/favorite_service.py
+++ backend/app/services/favorite_service.py
@@ -1,12 +1,23 @@
 from app.db.supabase import supabase
 
-
+# ネタ帳取得関数
 def get_favorites(user_id: str):
 
     response = supabase.table(
         "favorites"
     ).select("*").eq(
         "user_id", user_id
+    ).execute()
+
+    return response.data
+
+# ネタ帳登録関数
+def create_favorite(data: dict):
+
+    response = supabase.table(
+        "favorites"
+    ).insert(
+        data
     ).execute()
 
     return response.data
