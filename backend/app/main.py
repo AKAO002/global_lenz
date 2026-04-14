21ca96b944f5b3b87ead7dbd3582d1c9af5013d7
megane575 <rie-fttm0129@gmail.com>
2026-04-14 22:05:11 +0900

    add:corsの設定

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import time
 from app.config import SOURCES
 from app.logic.news_service import collect_headlines, fetch_media_articles
@@ -9,8 +10,20 @@ from app.app_api import router as api_router
 
 app = FastAPI()
 
-app.include_router(api_router)
+# --- CORS設定の追加_フロントエンドからのアクセスを許可します
+origins = [
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
 
 @app.get("/")
 def root():
